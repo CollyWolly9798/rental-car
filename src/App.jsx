2048db88fd4a5d72dea1fc2e552fe0631acff0cc
Oleksandr <collywolly9798@gmail.com>
2025-07-22 13:20:44 +0200

    add routes,change favicon and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import CatalogPage from './pages/CatalogPage/CatalogPage.jsx';
+import CarViewPage from './pages/CarViewPage/CarViewPage.jsx';
+import Layout from './components/Layout/Layout.jsx';
+
 function App() {
-  return <></>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/catalog/:id" element={<CarViewPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
---
+++ src/pages/CarViewPage/CarViewPage.jsx
@@ -0,0 +1,7 @@
+export default function CarViewPage() {
+  return (
+    <div>
+      <h1>Car View Page</h1>
+    </div>
+  );
+}
